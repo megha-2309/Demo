4525f4d39deadc02b9266ab24d0283490e4c873a
megha <meghabhalla2309@gmail.com>
2025-05-05 22:42:15 +0530

    initial commit

---
+++ javascript/1.Intro/first.js
@@ -0,0 +1,35 @@
+// console.log("Hello World");
+// console.log("Megha");
+
+// name="Tony Stark";
+// age=24;
+
+// x=null;
+// y=undefined;
+
+// console.log(name);
+// console.log(age);
+// console.log(x);
+// console.log(y);
+
+
+
+// const product={
+//     name:"Ball pen",
+//     rating:4,
+//     offer:5,
+//     price:270
+// };
+
+// console.log(product);
+
+const profile={
+    username:"Megha",
+    followers:200,
+    following:230,
+    isFollow:false
+};
+
+// console.log(profile);
+
+
